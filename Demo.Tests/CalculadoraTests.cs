4a51605ca0d73cf3892daeffcb9b7d031a662c73
Amanda Rodrigues <amanda.nrodrigues@hotmail.com>
2021-03-18 21:32:35 -0400

    Testando através de uma teoria - somar

--- Demo.Tests/CalculadoraTests.cs
+++ Demo.Tests/CalculadoraTests.cs
@@ -19,5 +19,24 @@ namespace Demo.Tests
             Assert.Equal(4,resultado);
 
         }
+
+        [Theory]
+        [InlineData(1,1,2)]
+        [InlineData(4, 4, 8)]
+        [InlineData(6, 6,12)]
+        [InlineData(8, 8, 16)]
+        [InlineData(100, 100, 200)]
+        public void Calculadora_Somar_RetornarValosSomaCorretos(double v1,double v2,double total)
+        {
+            //Arrange
+            var calculadora = new Calculadora();
+
+            //Act
+            var resultado = calculadora.Somar(v1, v2);
+
+            //Assert
+            Assert.Equal(total, resultado);
+
+        }
     }
 }
