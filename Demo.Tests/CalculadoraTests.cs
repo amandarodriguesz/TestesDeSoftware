356f0289c2a590c99499fc3121c58054b1b958e2
Amanda Rodrigues <amanda.nrodrigues@hotmail.com>
2021-03-18 21:06:04 -0400

    Adicionar arquivos de projeto.

---
+++ Demo.Tests/CalculadoraTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Demo.Tests
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void Calculadora_Somar_RetornarValorSoma()
+        {
+            //arrange
+            var calculadora = new Calculadora();
+            //Act
+            var resultado = calculadora.Somar(2, 2);
+
+            //Assert
+            Assert.Equal(4,resultado);
+
+        }
+    }
+}
---
+++ Demo/Calculadora.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo
+{
+    public class Calculadora
+    {
+        public double Somar(double v1,double v2)
+        {
+            return v1 + v2;
+        }
+        public int Dividir(int v1,int v2)
+        {
+            return v1 / v2;
+        }
+    }
+}
