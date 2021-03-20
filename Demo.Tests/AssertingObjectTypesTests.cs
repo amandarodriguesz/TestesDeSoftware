660fbd035cdd791facb8250a640f6706dbde2b34
Amanda Rodrigues <amanda.nrodrigues@hotmail.com>
2021-03-19 21:36:12 -0400

    Asserts para verificar tipo de obj

---
+++ Demo.Tests/AssertingObjectTypesTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Demo.Tests
+{
+    public class AssertingObjectTypesTests
+    {
+        [Fact]
+        public void FuncionarioFactory_Criar_DeveRetornarTipoFuncionario()
+        {
+            //Arrange e act
+            var funcionario = FuncionarioFactory.Criar("Amanda", 10000);
+
+            //Assert
+            Assert.IsType<Funcionario>(funcionario);
+        }
+
+        [Fact]
+        public void FuncionarioFactory_Criar_DeveRetornarTipoDerivadoPessoa()
+        {
+            //Arrange & Act
+            var funcionario = FuncionarioFactory.Criar("Amanda", 10000);
+
+            //Assert
+            Assert.IsAssignableFrom<Pessoa>(funcionario);
+        }
+    }
+}
